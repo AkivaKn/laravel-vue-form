9412868fc19a4383baedea91bbb4f31e4bfed5ac
AkivaKn <akivakaufman@gmail.com>
2024-08-07 12:32:24 +0100

    feat: add AuthController and login,register,logout
    Add methods with relevant API routes.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedUser = $request->validate([
+            'name' => 'required|max:100',
+            'email' => 'required|email:rfc,dns|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+        $user = User::create($validatedUser);
+        $token = $user->createToken($request->name);
+        return ['user' => $user, 'token' => $token->plainTextToken];
+    }
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email:rfc,dns|exists:users',
+            'password' => 'required',
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return ['message' => 'No such user.'];
+
+        } elseif (!Hash::check($request->password, $user->password)) {
+            return ['message' => 'Password is incorrect.'];
+        }
+
+        $token = $user->createToken($user->name);
+        return ['user' => $user, 'token' => $token->plainTextToken];
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return ['message' => 'Logout successful.'];
+
+    }
+}
