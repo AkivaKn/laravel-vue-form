df8cf369702a29b2c706bf12559af48c3be57fd5
AkivaKn <akivakaufman@gmail.com>
2024-08-07 12:39:53 +0100

    feat: add /user GET method to get logged in user

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,15 @@ use App\Http\Controllers\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
-Route::apiResource("customers",CustomerController::class);
+Route::apiResource("customers", CustomerController::class);
 
-Route::post('/register',[AuthController::class,'register']);
-Route::post('/login',[AuthController::class,'login']);
-Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
